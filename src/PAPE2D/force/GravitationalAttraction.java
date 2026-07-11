f4f15025cd18c7dcce02e58c09ecff23668156e1
CasperThePancake <60041702+CasperThePancake@users.noreply.github.com>
2026-07-11 18:56:17 +0200

    Added gravitational attraction force

---
+++ src/PAPE2D/force/GravitationalAttraction.java
@@ -0,0 +1,35 @@
+package PAPE2D.force;
+
+import PAPE2D.Body;
+import PAPE2D.LocalForce;
+import PAPE2D.helper.Vector2;
+
+public class GravitationalAttraction extends LocalForce {
+    private final double G;
+    private final Body body1;
+    private final Body body2;
+
+    /**
+     * Create a new gravitational attraction force
+     *
+     * @param body1 The first body in the relation
+     * @param body2 The second body in the relation
+     * @param G The value for G, the gravitational constant
+     */
+    public GravitationalAttraction(Body body1, Body body2, double G) {
+        this.body1 = body1;
+        this.body2 = body2;
+        this.G = G;
+    }
+
+    @Override
+    public void applyAcceleration(double dt) {
+        Vector2 connection = body2.getPosition().minus(body1.getPosition()).normalized(); // Vector from body 1 to body 2
+        double distance = body2.getPosition().distance(body1.getPosition());
+        double m1 = body1.getMass();
+        double m2 = body2.getMass();
+
+        body1.addVelocity(  connection.times(G * m1 * m2 / (distance * distance) * dt).times(body1.getInverseMass())  );
+        body2.addVelocity(  connection.times(-G * m1 * m2 / (distance * distance) * dt).times(body2.getInverseMass())  );
+    }
+}
